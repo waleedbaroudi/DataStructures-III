9045d189599645448604bb62a834df830c5ce6d9
waleedbaroudi <wbaroudi18@ku.edu.tr>
2020-04-20 16:02:12 +0300

    first try hash map dh

--- HashMapDH.java
+++ HashMapDH.java
@@ -1,10 +1,11 @@
 package code;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 import given.AbstractHashMap;
 import given.HashEntry;
-import given.iPrintable;
 
 /*
  * The file should contain the implementation of a hashmap with:
@@ -20,134 +21,161 @@ import given.iPrintable;
 
 public class HashMapDH<Key, Value> extends AbstractHashMap<Key, Value> {
 
-  // The underlying array to hold hash entries (see the HashEntry class)
-  private HashEntry<Key, Value>[] buckets;
+	// The underlying array to hold hash entries (see the HashEntry class)
+	private HashEntry<Key, Value>[] buckets;
 
-  //Do not forget to call this when you need to increase the size!
-  @SuppressWarnings("unchecked")
-  protected void resizeBuckets(int newSize) {
-    // Update the capacity
-    N = nextPrime(newSize);
-    buckets = (HashEntry<Key, Value>[]) Array.newInstance(HashEntry.class, N);
-  }
+	// Do not forget to call this when you need to increase the size!
+	@SuppressWarnings("unchecked")
+	protected void resizeBuckets(int newSize) {
+		// Update the capacity
+		N = nextPrime(newSize);
+		buckets = (HashEntry<Key, Value>[]) Array.newInstance(HashEntry.class, N);
+	}
 
-  // The threshold of the load factor for resizing
-  protected float criticalLoadFactor;
+	// The threshold of the load factor for resizing
+	protected float criticalLoadFactor;
 
-  // The prime number for the secondary hash
-  int dhP;
+	// The prime number for the secondary hash
+	int dhP;
 
-  /*
-   * ADD MORE FIELDS IF NEEDED
-   * 
-   */
+	/*
+	 * ADD MORE FIELDS IF NEEDED
+	 * 
+	 */
 
-  /*
-   * ADD A NESTED CLASS IF NEEDED
-   * 
-   */
+	/*
+	 * ADD A NESTED CLASS IF NEEDED
+	 * 
+	 */
 
-  // Default constructor
-  public HashMapDH() {
-    this(101);
-  }
+	// Default constructor
+	public HashMapDH() {
+		this(101);
+	}
 
-  public HashMapDH(int initSize) {
-    this(initSize, 0.6f);
-  }
+	public HashMapDH(int initSize) {
+		this(initSize, 0.6f);
+	}
 
-  public HashMapDH(int initSize, float criticalAlpha) {
-    N = initSize;
-    criticalLoadFactor = criticalAlpha;
-    resizeBuckets(N);
+	public HashMapDH(int initSize, float criticalAlpha) {
+		N = initSize;
+		criticalLoadFactor = criticalAlpha;
+		resizeBuckets(N);
 
-    // Set up the MAD compression and secondary hash parameters
-    updateHashParams();
+		// Set up the MAD compression and secondary hash parameters
+		updateHashParams();
 
-    /*
-     * ADD MORE CODE IF NEEDED
-     * 
-     */
-  }
+		/*
+		 * ADD MORE CODE IF NEEDED
+		 * 
+		 */
+	}
 
-  /*
-   * ADD MORE METHODS IF NEEDED
-   * 
-   */
+	private int getEntryIndex(Key k) {
+		for (int i = 0; i < N; i++) {
+			int index = hashValue(k, i);
+			if (k.equals(buckets[index]))
+				return index;
+		}
+		return -1;
+	}
 
-  /**
-   * Calculates the hash value by compressing the given hashcode. Note that you
-   * need to use the Multiple-Add-Divide method. The class variables "a" is the
-   * scale, "b" is the shift, "mainP" is the prime which are calculated for you.
-   * Do not include the size of the array here
-   * 
-   * Make sure to include the absolute value since there maybe integer overflow!
-   */
-  protected int primaryHash(int hashCode) {
-    // TODO: Implement MAD compression given the hash code, should be 1 line
-    return Math.abs(-1);
-  }
+	private int findEmptyBucket(Key k) {
+		int index = 0;
+		int probe = 0;
+		do {
+			index = hashValue(k, probe);
+			probe++;
+		} while (buckets[index] != null);
+		return index;
+	}
 
-  /**
-   * The secondary hash function. Remember you need to use "dhP" here!
-   * 
-   */
-  protected int secondaryHash(int hashCode) {
-    // TODO: Implement the secondary hash function taught in the class
-    return -1;
-  }
+	/**
+	 * Calculates the hash value by compressing the given hashcode. Note that you
+	 * need to use the Multiple-Add-Divide method. The class variables "a" is the
+	 * scale, "b" is the shift, "mainP" is the prime which are calculated for you.
+	 * Do not include the size of the array here
+	 * 
+	 * Make sure to include the absolute value since there maybe integer overflow!
+	 */
+	protected int primaryHash(int hashCode) {
+		// TODO: Implement MAD compression given the hash code, should be 1 line
+		return Math.abs(((a * hashCode) + b) % P);
+	}
 
-  @Override
-  public int hashValue(Key key, int iter) {
-    int k = Math.abs(key.hashCode());
-    return Math.abs(primaryHash(k) + iter * secondaryHash(k)) % N;
-  }
+	/**
+	 * The secondary hash function. Remember you need to use "dhP" here!
+	 * 
+	 */
+	protected int secondaryHash(int hashCode) {
+		return dhP - (hashCode % dhP);
+	}
 
-  /**
-   * checkAndResize checks whether the current load factor is greater than the
-   * specified critical load factor. If it is, the table size should be increased
-   * to 2*N and recreate the hash table for the keys (rehashing). Do not forget to
-   * re-calculate the hash parameters and do not forget to re-populate the new
-   * array!
-   */
-  protected void checkAndResize() {
-    if (loadFactor() > criticalLoadFactor) {
-      // TODO: Fill this yourself
+	@Override
+	public int hashValue(Key key, int iter) {
+		int k = Math.abs(key.hashCode());
+		return Math.abs(primaryHash(k) + iter * secondaryHash(k)) % N;
+	}
 
-    }
-  }
+	/**
+	 * checkAndResize checks whether the current load factor is greater than the
+	 * specified critical load factor. If it is, the table size should be increased
+	 * to 2*N and recreate the hash table for the keys (rehashing). Do not forget to
+	 * re-calculate the hash parameters and do not forget to re-populate the new
+	 * array!
+	 */
+	protected void checkAndResize() {
+		if (loadFactor() > criticalLoadFactor) {
+			// TODO: Fill this yourself
 
-  @Override
-  public Value get(Key k) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+		}
+	}
 
-  @Override
-  public Value put(Key k, Value v) {
-    // TODO Auto-generated method stub
-    // Do not forget to resize if needed!
-    return null;
-  }
+	@Override
+	public Value get(Key k) {
+		for (int i = 0; i < N; i++) {
+			int index = hashValue(k, i);
+			if (k.equals(buckets[index]))
+				return buckets[index].getValue();
+		}
 
-  @Override
-  public Value remove(Key k) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+		return null;
+	}
 
-  // This is the only function you are allowed to use an existing Java data
-  // structure!
-  @Override
-  public Iterable<Key> keySet() {
-    // TODO Auto-generated method stub
-    return null;
-  }
+	@Override
+	public Value put(Key k, Value v) {
+		checkAndResize();
+		int putIndex = getEntryIndex(k);
+		if (putIndex != -1) {
+			Value temp = buckets[putIndex].getValue();
+			buckets[putIndex].setValue(v);
+			return temp;
+		}
+		buckets[findEmptyBucket(k)] = new HashEntry<>(k, v);
+		return null;
+	}
 
-  @Override
-  protected void updateHashParams() {
-    super.updateHashParams();
-    dhP = nextPrime(N / 2);
-  }
+	@Override
+	public Value remove(Key k) {
+		int removeIndex = getEntryIndex(k);
+		if (removeIndex == -1)
+			return null;
+		Value temp = buckets[removeIndex].getValue();
+		buckets[removeIndex] = null;
+		return temp;
+	}
+
+	// This is the only function you are allowed to use an existing Java data
+	// structure!
+	@Override
+	public Iterable<Key> keySet() {
+		return Arrays.stream(buckets).map(x -> x.getKey()).collect(Collectors.toSet());
+	}
+
+	@Override
+	protected void updateHashParams() {
+		super.updateHashParams();
+		dhP = nextPrime(N / 2);
+	}
 
 }
